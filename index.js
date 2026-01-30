0862073dd14775f3f9d3ff7dbcd6e24bf114a67e
Riya Yadav <riyaaradhya8@gmail.com>
2026-01-31 02:47:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import chatbotRoutes from './routes/chatbot.route.js';
+const app = express()
+dotenv.config()
+const port = process.env.PORT ||3000
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+// Database Connection Code
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB")
+}).catch((error) => {
+    console.log("error connecting to MongoDB:", error)
+})
+
+// Defining Routes
+
+app.use("/bot/v1/", chatbotRoutes)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bot from './components/Bot'
+
+
+
+
+function App() {
+  return (
+    <div><Bot/></div>
+  )
+}
+
+export default App
